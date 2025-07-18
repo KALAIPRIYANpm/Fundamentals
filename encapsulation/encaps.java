45eab5b62a003e07f2e9916b170d8b8fcc3f8c40
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-07-18 14:27:20 +0530

    encapsulation

---
+++ encapsulation/encaps.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+class enca{
+    private String name;
+    public void setName(String setname){
+        name = setname;   
+    }
+    public String  getName(){
+        return name;
+    }
+}
+
+
+public class encaps {
+    public static void main(String[] args) {
+        enca object = new enca();
+        object.setName("Kalaipriyan");
+        System.out.println(object.getName());
+    }
+}
